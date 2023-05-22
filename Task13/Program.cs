e7cbfb5a084b4fd64754a8262ebf21e5e7d60b98
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-22 11:44:26 +0300

    somehow completed Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+if (Num < 99)
+    Console.WriteLine("Третьей цифры нет");
+else
+{
+while (Num > 999)
+    Num = Num / 10;
+Num = Num % 10;
+Console.WriteLine($"{Num} - третье число");
+
+}
