45e1350e65026470e9a364df918481d11e56c02e
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-22 11:52:43 +0300

    Completed Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите цифру дня недели: ");
+int Num = int.Parse(Console.ReadLine()!);
+if (Num == 6 || Num == 7)
+    Console.WriteLine("Это выходной");
+else
+    Console.WriteLine("Это не выходной");
