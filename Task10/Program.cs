6a3dd1364eee7d44062eb38260416fff0d73e20a
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-22 11:22:31 +0300

    Completed Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+if (Num < 99 || Num > 999 )
+    Console.WriteLine("Число не трёхзначное");
+else
+    {
+    Num = (Num/10)%10;
+    Console.Write($"{Num} - вторая цифра");
+    }
